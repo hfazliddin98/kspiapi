deb3e35331201ec2c3080a8f3e59d70f6fef1c4f
hfazliddin98 <hatamovfazliddin505@gmail.com>
2024-03-29 11:38:45 +0500

    29.3.2024

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import YangilikListAPIView, YangilikCreateAPIView, YangilikUpdateAPIView, YangilikDestroyAPIView
-from .views import ElonView, GalareyaTuriView, GalareyaView
+from .views import ElonListAPIView, ElonCreateAPIView, ElonUpdateAPIView, ElonDestroyAPIView
+from .views import GalareyaTuriListAPIView, GalareyaTuriCreateAPIView, GalareyaTuriUpdateAPIView, GalareyaTuriDestroyAPIView
+from .views import GalareyaListAPIView, GalareyaCreateAPIView, GalareyaUpdateAPIView, GalareyaDestroyAPIView
 
 urlpatterns = [
     path('yangilik/get/', YangilikListAPIView.as_view()),
@@ -9,7 +11,20 @@ urlpatterns = [
     path('yangilik/delete/', YangilikDestroyAPIView.as_view()),
 
     # elon uchun
-    path('elon/', ElonView.as_view()),
-    path('galareya/tur/', GalareyaTuriView.as_view()),
-    path('galareya/', GalareyaView.as_view()),
+    path('elon/get/', ElonListAPIView.as_view()),
+    path('elon/post/', ElonCreateAPIView.as_view()),
+    path('elon/update/', ElonUpdateAPIView.as_view()),
+    path('elon/delete/', ElonDestroyAPIView.as_view()),
+
+    #  galareya turi
+    path('galareya/tur/get/', GalareyaTuriListAPIView.as_view()),
+    path('galareya/tur/post/', GalareyaTuriCreateAPIView.as_view()),
+    path('galareya/tur/update/', GalareyaTuriUpdateAPIView.as_view()),
+    path('galareya/tur/delete/', GalareyaTuriDestroyAPIView.as_view()),
+
+    # galareya
+    path('galareya/get/', GalareyaListAPIView.as_view()),
+    path('galareya/post/', GalareyaCreateAPIView.as_view()),
+    path('galareya/update/', GalareyaUpdateAPIView.as_view()),
+    path('galareya/delete/', GalareyaDestroyAPIView.as_view()),
 ]
--- home/views.py
+++ home/views.py
@@ -29,20 +29,79 @@ class YangilikDestroyAPIView(DestroyAPIView):
     queryset = Yangilik.objects.all()
 
 
-class ElonView(ListCreateAPIView):
+# elon 
+    
+class ElonListAPIView(ListAPIView):
+    permission_classes = [AllowAny]
     metadata_class = Elon
     serializer_class = ElonSerializer
     queryset = Elon.objects.all()
 
 
-class GalareyaTuriView(ListCreateAPIView):
+class ElonCreateAPIView(CreateAPIView):
+    metadata_class = Elon
+    serializer_class = ElonSerializer
+    queryset = Elon.objects.all()
+
+class ElonUpdateAPIView(UpdateAPIView):
+    metadata_class = Elon
+    serializer_class = ElonSerializer
+    queryset = Elon.objects.all()
+
+
+class ElonDestroyAPIView(DestroyAPIView):
+    metadata_class = Elon
+    serializer_class = ElonSerializer
+    queryset = Elon.objects.all()
+
+
+# galareya turi
+
+class GalareyaTuriListAPIView(ListAPIView):
+    permission_classes = [AllowAny]
     metadata_class = GalareyaTuri
     serializer_class = GalareyaTuriSerializer
     queryset = GalareyaTuri.objects.all()
 
 
-class GalareyaView(ListCreateAPIView):
+class GalareyaTuriCreateAPIView(CreateAPIView):
+    metadata_class = GalareyaTuri
+    serializer_class = GalareyaTuriSerializer
+    queryset = GalareyaTuri.objects.all()
+
+class GalareyaTuriUpdateAPIView(UpdateAPIView):
+    metadata_class = GalareyaTuri
+    serializer_class = GalareyaTuriSerializer
+    queryset = GalareyaTuri.objects.all()
+
+
+class GalareyaTuriDestroyAPIView(DestroyAPIView):
+    metadata_class = GalareyaTuri
+    serializer_class = GalareyaTuriSerializer
+    queryset = GalareyaTuri.objects.all()
+
+
+# galareya
+
+class GalareyaListAPIView(ListAPIView):
+    permission_classes = [AllowAny]
     metadata_class = Galareya
     serializer_class = GalareyaSerializer
     queryset = Galareya.objects.all()
 
+
+class GalareyaCreateAPIView(CreateAPIView):
+    metadata_class = Galareya
+    serializer_class = GalareyaSerializer
+    queryset = Galareya.objects.all()
+
+class GalareyaUpdateAPIView(UpdateAPIView):
+    metadata_class = Galareya
+    serializer_class = GalareyaSerializer
+    queryset = Galareya.objects.all()
+
+
+class GalareyaDestroyAPIView(DestroyAPIView):
+    metadata_class = Galareya
+    serializer_class = GalareyaSerializer
+    queryset = Galareya.objects.all()
